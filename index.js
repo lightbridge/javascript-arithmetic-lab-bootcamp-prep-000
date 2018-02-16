8ab43e55c9f1a3bd3432b4bfc75a0dbb2b6d16a4
Chad Bishop <connect@ltbrdg.com>
2018-02-16 00:46:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function add(){
 function subtract(){
  return 3 - 2;
 }
-function add(){
+function multiply(){
  return 1 + 2;
 }
-function add(){
+function divide(){
  return 1 + 2;
 }function add(){
  return 1 + 2;
